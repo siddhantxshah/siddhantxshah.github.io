9971a9825721f7487d21ee10570484f55c0dbf14
Siddhant Shah <siddhant@siddhant.design>
2016-09-26 15:36:23 -0400

    updated when

--- module.js
+++ module.js
@@ -2,10 +2,13 @@ var uiApp = angular.module('uiApp',['ngRoute']);
 
 uiApp.config(function($routeProvider){
   $routeProvider
+  .when('/',{
+    templateUrl: 'Views/ContentView/ContentView.html'
+  })
   .when("/content",{
       templateUrl: 'Views/ContentView/ContentView.html'
   })
-  .otherwise('/content');
+  .otherwise('/');
 
 
 
