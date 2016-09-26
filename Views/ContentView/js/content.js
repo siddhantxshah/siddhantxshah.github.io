50cff1d313c85b91b61e8c30fb361af19d13be81
Siddhant Shah <siddhant@siddhant.design>
2016-09-26 13:14:50 -0400

    content

--- Views/ContentView/js/content.js
+++ Views/ContentView/js/content.js
@@ -2,7 +2,7 @@ var contentApp = angular.module('contentApp',['ngRoute']);
 
 contentApp.config(function($routeProvider){
   $routeProvider
-  .when("/contentPageOne",{
+  .when("/content/contentPageOne",{
     templateUrl: 'UI_Website/Views/ContentView/pageOne.html'
   })
   .otherwise("/contentPageOne");
