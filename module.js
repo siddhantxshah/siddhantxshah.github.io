d3b98c91b4fb8bde93c4e9f0600bfbc72ac2b87f
Siddhant Shah <siddhant@siddhant.design>
2016-09-26 15:37:26 -0400

    added

--- module.js
+++ module.js
@@ -3,7 +3,7 @@ var uiApp = angular.module('uiApp',['ngRoute']);
 uiApp.config(function($routeProvider){
   $routeProvider
   .when('/',{
-    templateUrl: 'Views/ContentView/ContentView.html'
+    templateUrl: 'Views/home.html'
   })
   .when("/content",{
       templateUrl: 'Views/ContentView/ContentView.html'
